98aa70632d03690f538409a13233f94230dd8961
dingqz <d362408870@gmail.com>
2016-09-28 18:14:01 +0800

    三目运算符

---
+++ 20160924/TestThree.java
@@ -0,0 +1,21 @@
+public class TestThree
+{
+       public static void main(String[] args)
+      {
+            String str = 5 > 3 ? "5大于3" : "5不大于3" ;
+            System. out.println(str);
+
+            String str2 = null;
+             if (5 > 3)
+            {
+                  str2 = "5大于3";
+            }
+             else
+             {
+                  str2 = "5不大于3" ;
+            }
+      }
+}
+
+
+//5大于3
