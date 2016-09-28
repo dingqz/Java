48b95577e79ddaa83a2529da66697b61aeb85c77
dingqz <d362408870@gmail.com>
2016-09-28 19:56:18 +0800

    new关键字调用Person类的构造器，返回一个Person实例

---
+++ 20160925/Person.java
@@ -0,0 +1,12 @@
+public class Person
+{
+       //下面定义了Person类的两个属性
+       public String name ;
+       public int age ;
+
+       //下面定义了一个say方法
+       public void say(String content)
+      {
+            System. out.println(content);
+      }
+}
