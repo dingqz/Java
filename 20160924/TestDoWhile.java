6077fc4008f7c562d8a9b67f8a78ec3bc818bac3
dingqz <d362408870@gmail.com>
2016-09-28 18:20:24 +0800

    while循环     do while循环

---
+++ 20160924/TestDoWhile.java
@@ -0,0 +1,42 @@
+public class TestDoWhile
+{
+       public static void main(String[] args)
+      {
+
+             //定义变量count
+             int count = 1;
+             //执行do while循环
+             do
+            {
+                  System. out.println(count);
+                   // 循环迭代语句
+                  count++;
+            }
+             //循环条件紧跟while关键字
+             while (count < 10);
+            System. out.println("循环结束!" );
+
+             //定义变量count2
+             int count2 = 20;
+             //执行do while循环
+             do
+                   //这行代码把循环体和迭代部分合并成了一行代码
+                  System. out.println(count2++);
+             while (count2 < 10);
+            System. out.println("循环结束!" );
+      }
+}
+
+
+//1
+//2
+//3
+//4
+//5
+//6
+//7
+//8
+//9
+//循环结束!
+//20
+//循环结束!
