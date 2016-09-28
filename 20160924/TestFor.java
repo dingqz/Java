f1f2f77c4228b569e52bc89a55915e8e831ee1d2
dingqz <d362408870@gmail.com>
2016-09-28 18:21:56 +0800

    for循环

---
+++ 20160924/TestFor.java
@@ -0,0 +1,25 @@
+public class TestFor
+{
+       public static void main(String[] args)
+      {
+             //循环的初始化条件,循环条件，循环迭代语句都在下面一行
+             for (int count = 0 ; count < 10 ; count++)
+            {
+                  System. out.println(count);          
+            }
+            System. out.println("循环结束!" );
+      }
+}
+
+
+//0
+//1
+//2
+//3
+//4
+//5
+//6
+//7
+//8
+//9
+//循环结束!
