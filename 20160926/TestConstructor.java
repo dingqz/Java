c6d8859e8f5232dff7b236deca64bb029ebb0c0f
dingqz <d362408870@gmail.com>
2016-09-28 20:43:00 +0800

    该构造器包含两个参数

---
+++ 20160926/TestConstructor.java
@@ -0,0 +1,26 @@
+public class TestConstructor
+{
+       public String name ;
+       public int count ;
+       //提供自定义的构造器，该构造器包含两个参数
+       public TestConstructor(String name , int count)
+      {
+             //构造器里的this代表它进行初始化的对象
+             //下面两行代码将传入的2个参数赋给this代表对象的name和count属性
+             this.name = name;
+             this.count = count;
+      }
+       public static void main(String[] args)
+      {
+             //使用自定义的构造器来创建TestConstructor对象
+             //系统将会对该对象执行自定义的初始化
+            TestConstructor tc = new TestConstructor("Struts2权威指南" , 20000);
+             //输出TestConstructor对象的name和count属性
+            System. out.println(tc.name );
+            System. out.println(tc.count );
+      }
+}
+  
+
+//Struts2权威指南
+//20000
