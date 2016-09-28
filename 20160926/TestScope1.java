e9fc2a7edb0fb9b8b4bea622a27f9c1aaa9833dd
dingqz <d362408870@gmail.com>
2016-09-28 20:33:28 +0800

    成员变量 局部变量 代码块局部变量 作为循环变量

---
+++ 20160926/TestScope1.java
@@ -0,0 +1,23 @@
+public class TestScope1
+{
+       //定义一个成员变量作为循环变量
+       static int i;
+       public static void main(String[] args)
+      {
+             for ( i = 0 ; i < 10 ; i++)
+            {
+                  System. out.println("Hello" );
+            }
+      }
+}
+
+//Hello
+//Hello
+//Hello
+//Hello
+//Hello
+//Hello
+//Hello
+//Hello
+//Hello
+//Hello
