8e61879b6f7cd64e641fa34cce9cf8ca86c661c1
dingqz <d362408870@gmail.com>
2016-09-28 20:39:36 +0800

    import static 静态引用

---
+++ 20160926/TestStaticImport.java
@@ -0,0 +1,17 @@
+
+import static java.lang.System.*;
+import static java.lang.Math.*;
+
+
+public class TestStaticImport
+{
+       public static void main(String[] args)
+      {
+             //out是java.lang.System类的静态属性，代表标准输出
+             //PI是java.lang.Math类的静态属性，表示常量
+             out.println(PI);
+      }
+}
+
+
+//3.141592653589793
