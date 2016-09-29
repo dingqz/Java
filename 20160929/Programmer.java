26ccf2ee85317091480b7e323a4305b7746cd687
dingqz <d362408870@gmail.com>
2016-09-29 09:56:12 +0800

    创建与Programer类有相同的work方法的Teachable接口  保证teacher的work
    //返回一个非静态内部类引用，允许外部类通过该非静态内部类引用来回调外部类的方法
    public Teachable getCallbackReference()
    {
    return new Closure();
    }

---
+++ 20160929/Programmer.java
@@ -0,0 +1,26 @@
+interface Teachable
+{
+       void work();
+}
+public class Programmer
+{
+       private String name ;
+       //Programmer类的两个构造器
+       public Programmer(){}
+       public Programmer(String name)
+      {
+             this.name = name;
+      }
+       public void setName(String name)
+      {
+             this.name = name;
+      }
+       public String getName()
+      {
+             return this .name ;
+      }
+       public void work()
+      {
+            System. out.println(name + "在灯下认真敲键盘...");
+      }
+}
